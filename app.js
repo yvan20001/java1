ac45d2e2aa4d33d1aac52a1e0aa2b1760296e8ed
yvan1 <you@example.com>
2021-01-15 17:31:14 +0100

    ::i#

---
+++ app.js
@@ -0,0 +1,25 @@
+const1="gmail.com";
+dato1=prompt("Dni");
+dato2=prompt("Nombre");
+dato3=prompt("Apellido1" ,"apellido2");
+dato4=prompt("Data de nacimiento")
+
+
+
+dato6=prompt("cuenta de corriente")
+dato7=prompt("tipo de jugador")
+
+
+edad =prompt("dime tu edad");
+
+if(16<=edad <18){alert("tu categoria es amateur")}
+
+else if(edad >=18){alert("tu categoria es profesional")}
+
+dato5=prompt("correo electronico  +" + const1 +" Solo apto correos acabados en gmail.com")
+
+    
+dato5=prompt8  ("")
+
+
+datos[" (NIF, nom, llinatges, data de naixement, telèfon, email, compte corrent per cobrar la quota del torneig  i tipus de jugador"]
